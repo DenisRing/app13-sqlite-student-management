352276c18a52f9771a681097673299b9a2f94ec4
Denis <denisring@protonmail.com>
2023-05-12 14:30:17 +0100

    Finishing

---
+++ skelframe.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
+     QLineEdit, QPushButton, QMainWindow
+import sys
+from datetime import datetime
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+
+
+app = QApplication(sys.argv)
+age_calculator = MainWindow()
+age_calculator.show()
+sys.exit(app.exec())
